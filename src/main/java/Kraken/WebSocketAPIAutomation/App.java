26ed8a0010082b538101995f8305a83aee0af556
Tinu Sany <tinu.sany@gmail.com>
2022-04-18 13:45:54 +0100

    first commit

---
+++ src/main/java/Kraken/WebSocketAPIAutomation/App.java
@@ -0,0 +1,13 @@
+package Kraken.WebSocketAPIAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
